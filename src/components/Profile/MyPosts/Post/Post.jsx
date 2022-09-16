00a8ca63a00996935df099ce803af01887053343
Arthur <a.sukhozhenko@gmail.com>
2022-09-16 12:17:58 +0300

    update

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -6,10 +6,10 @@ const Post = (props) => {
   return (
     <div>
       <div className={s.item}>
-        <img src="https://shapka-youtube.ru/wp-content/uploads/2021/02/prikolnaya-avatarka-dlya-patsanov.jpg" alt="" />
+        <img src="https://www.volynpost.com/img/modules/news/a/2d/89c6db59bcc5faedda072e84c6e432da/cb-yz2dikefvsy.jpg" alt="" />
         {props.message}
         <div>
-          <span>Like</span> { props.likeCount }
+          <span>Вподобайки</span> { props.likeCount }
         </div>
       </div>
     </div>
