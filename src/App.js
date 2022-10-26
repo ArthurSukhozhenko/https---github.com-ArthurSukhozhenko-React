54053d0c1558ff70973219114cdb34e2d72b5caa
Arthur <a.sukhozhenko@gmail.com>
2022-10-26 12:51:36 +0300

    qfix

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Dialogs from "./components/Dialogs/Dialogs";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 const App = (props) => {
   return (
